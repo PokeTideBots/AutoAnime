4d166a6102510348a644d81c057e6cd2151f0abc
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2023-01-21 16:13:51 +0530

    Update __init__.py
    Signed-off-by: PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -13,13 +13,13 @@ LOG.setLevel(level=logging.INFO)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
 
-API_ID = int(environ.get('API_ID', None)) #API ID
-API_HASH = environ.get('API_HASH', None) #API HASH
-BOT_TOKEN = environ.get('BOT_TOKEN', None) #BOT TOKEN
-DATABASE_URL = environ.get('DATABASE_URL', None) #MONGO DB
-OWNER_ID = int(environ.get('OWNER_ID', None)) #OWNER ID
-MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', None))#YOUR MAIN CHANNEL ID
-ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', None))#YOUR ARCHIVE CHANNEL
-MESSAGE_ID = int(environ.get('MESSAGE_ID', None)) #SUB CHANNEL STATUS ID
+API_ID = int(environ.get('API_ID', 4783634)) #API ID
+API_HASH = environ.get('API_HASH', "f6c33f46599246676f75e153b615dbbc") #API HASH
+BOT_TOKEN = environ.get('BOT_TOKEN', "5872952946:AAHVZRDj2wIY_KWjAWbXfM2vAp5lOmRcAsQ") #BOT TOKEN
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://Devilharsha:Devilharsha@cluster0.exwkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority") #MONGO DB
+OWNER_ID = int(environ.get('OWNER_ID', 1165699179) #OWNER ID
+MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', -1001763302512))#YOUR MAIN CHANNEL ID
+ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', -1001645644921))#YOUR ARCHIVE CHANNEL
+MESSAGE_ID = int(environ.get('MESSAGE_ID', 76)) #SUB CHANNEL STATUS ID
 
 soheru = Client('SoheruBots', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
